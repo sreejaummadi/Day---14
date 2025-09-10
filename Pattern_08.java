98641e85edbbfe82b3261aadd97c52ebcf61bca5
Ummadi Sreeja <157631852+sreejaummadi@users.noreply.github.com>
2025-09-10 12:47:08 +0530

    Assessment - 01

---
+++ Pattern_08.java
@@ -0,0 +1,21 @@
+package com.codegnan.Patterns;
+
+public class Pattern_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 7; 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1 || i == 0 || i == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+	}
+
+}
